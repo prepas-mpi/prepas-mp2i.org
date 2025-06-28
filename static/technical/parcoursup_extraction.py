5fda96fb653303794b3af6d5965d193f2ed86cb2
LarsenWood <valentinrobin@icloud.com>
2025-06-28 14:59:28 +0200

    feat: add many (too much) comments

--- static/technical/parcoursup_extraction.py
+++ static/technical/parcoursup_extraction.py
@@ -1,19 +1,27 @@
+# Programme de data-scrapping Parcoursup, session 2024.
+# Modules requis non-natifs : requests
+
 import requests
 from datetime import datetime
 
 try:
+    # Ensemble du jeu de données Parcoursup
     url = "https://data.education.gouv.fr/api/explore/v2.1/catalog/datasets/fr-esr-parcoursup/records"
+    # Paramètres de requête, limite à 100 (car maximum possible) et refine pour récupérer uniquement les données où fil_lib_voe_acc = "MP2I"
     params = {
         "limit": 100,
         "refine": 'fil_lib_voe_acc:"MP2I"'
     }
 
+    # Requête HTTP GET pour récupérer les données
     response = requests.get(url, params=params)
     response.raise_for_status()
+    # Le résultat est une réponse JSON
     data = response.json()
 
     records = data.get("results", [])
 
+    # On initialise différentes variables pour compter les éléments que nous recherchons
     total_places = 0
     voeux_total = 0
     propositions_total = 0
@@ -25,6 +33,8 @@ try:
     count_rang = 0
     count_taux = 0
 
+    # On boucle à travers les données en récupérant les différents champs qui nous intéresse. 
+    # La méthodologie est disponible ici pour mieux comprendre : https://data.education.gouv.fr/api/explore/v2.1/catalog/datasets/fr-esr-parcoursup/attachments/methodologie_opendata_2024_pdf
     for rec in records:
         if rec.get("capa_fin") is not None:
             total_places += rec["capa_fin"]
@@ -41,12 +51,14 @@ try:
             taux_total += rec["taux_acces_ens"]
             count_taux += 1
 
+    # On calcule les moyennes puisque nos 4 dernières informations sont des moyennes.
     moyenne_voeux = round(voeux_total / count_voeux) if count_voeux else 0
     moyenne_prop = round(propositions_total / count_prop) if count_prop else 0
     moyenne_rang = round(rang_total / count_rang) if count_rang else 0
     moyenne_taux = round(taux_total / count_taux, 1) if count_taux else 0
 
 
+    # On fini par afficher les données tel que nécessaire pour simplement les copier/coller sur la page Markdown du site (filiere.md)
     print(f"""Voici les chiffres cumulés pour l'année {datetime.now().year - 1} : 
 
 - Nombre de places : {total_places}
